df961e29895ccb7f8c0ce8f57bfeb3c12007ef5f
Amit Naipaul <amitnaipaul@gmail.com>
2021-01-20 15:52:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,3 +15,4 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   planeteer_calls.find { |planeteer_calls| planeteer_calls == "Earth!" || planeteer_calls == "Wind!" }
+end
