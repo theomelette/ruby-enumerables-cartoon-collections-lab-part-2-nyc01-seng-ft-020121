dc101c747d433e4641da3e754f0349e9642f0680
Amit Naipaul <amitnaipaul@gmail.com>
2021-01-20 15:51:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,5 +14,5 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  planeteer_calls.find { |planeteer_calls| planeteer_calls == "Earth!" || planeteer_calls = "Wind!" }
+  planeteer_calls.find { |planeteer_calls| planeteer_calls == "Earth!" || planeteer_calls = "Wood!" }
 end
